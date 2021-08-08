64395994eda0514a0472b2dbab9ee98597e1eb00
iqaenganer <daw.camalian@yandex.ru>
2021-08-08 14:15:48 +0300

    CommitFixBug

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -10,7 +10,7 @@ public class StatisticsService {
   public long findMax(long[] incomes) {
     long currentMax = incomes[0];
     for (long income : incomes){
-      if  (currntMax < income)
+      if  (currentMax < income)
         currentMax = income;
       }
         return currentMax;
