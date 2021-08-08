ae3c52c252377fc5725e66d1bab2765bd54bd796
iqaenganer <daw.camalian@yandex.ru>
2021-08-08 12:47:06 +0300

    FailCOn

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,7 +10,7 @@ class StatisticsServiceTest {
   void findMax() {
     StatisticsService service = new StatisticsService();
 
-    long[] incomesInBillions = {2,9, 5, 8, 4, 5, 3, 8, 7, 11, 10, 12,16};
+    long[] incomesInBillions = {16,9, 5, 8, 4, 5, 3, 8, 7, 11, 10, 12,16};
     long expected = 16;
 
     long actual = service.findMax(incomesInBillions);
