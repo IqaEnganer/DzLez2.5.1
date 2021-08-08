d1d60a91d4bf285bf84a6873c22e87bfc9980c05
iqaenganer <daw.camalian@yandex.ru>
2021-08-08 14:10:55 +0300

    failCode

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -10,7 +10,7 @@ public class StatisticsService {
   public long findMax(long[] incomes) {
     long currentMax = incomes[0];
     for (long income : incomes){
-      if  (currentMax < income)
+      if  (currntMax < income)
         currentMax = income;
       }
         return currentMax;
